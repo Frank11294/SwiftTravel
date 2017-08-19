fead48fceb12b459a697d4f6ea9d982501bef860
Frank Roman Izaguirre <franker11294@gmail.com>
2017-08-19 03:26:57 +0200

    mac

--- app/src/main/java/com/example/frankernesto/proyecto/CrearViajeActivity2.java
+++ app/src/main/java/com/example/frankernesto/proyecto/CrearViajeActivity2.java
@@ -4,11 +4,13 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ImageSwitcher;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 
 import com.google.android.gms.location.places.PlacePhotoMetadata;
 import com.google.android.gms.location.places.GeoDataApi;
+import com.squareup.picasso.Picasso;
 
 
 public class CrearViajeActivity2 extends AppCompatActivity {
@@ -19,38 +21,30 @@ public class CrearViajeActivity2 extends AppCompatActivity {
     private String id , Nom;
     private TextView nombreLoc;
     private GeoDataApi mGeoDataApi;
+    private final String API_KEY="AIzaSyD68b2rZ58lgs4ZsdoSgFaYtLYyIVnb7dE";
+
+    private ImageView imagen;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_crear_viaje2);
 
+       // imagen = (ImageView) findViewById(R.id.imageView2);
+
         intent=getIntent();
         id=intent.getStringExtra("ID");
         Nom=intent.getStringExtra("NOMBRE");
 
 
+        String url = String.format("https://maps.googleapis.com/maps/api/place/photo?maxWidth=%d&photoreference=%s&key=%s",200,id,API_KEY);
 
-//        nombreLoc = (TextView) findViewById(R.id.textView);
-//        nombreLoc.setText("id :"+id+" Nombre :"+Nom);
+        Picasso.with(this).load("https://www.google.es/search?q=random+photo&tbm=isch&imgil=ashA-nYuhh7bAM%253A%253BWXKPgRO8rZhp1M%253Bhttp%25253A%25252F%25252Fwww.thedesignwork.com%25252F40-random-pictures-of-conceptual-and-creative-ideas%25252F&source=iu&pf=m&fir=ashA-nYuhh7bAM%253A%252CWXKPgRO8rZhp1M%252C_&usg=__LGg6-y7IIWdO2moxZdT991f9TNA%3D&biw=1324&bih=932&ved=0ahUKEwjttubZkOLVAhUFExoKHedJDRcQyjcIQw&ei=PZKXWa2kOYWmaOeTtbgB#imgrc=ashA-nYuhh7bAM:").into(imagen);
 
-//        sw = (ImageSwitcher)findViewById(R.id.imgsw);
-//
-//        sw.setFactory(new ViewSwitcher.ViewFactory() {
-//            @Override
-//            public View makeView() {
-//                ImageView imageView = new ImageView(getApplicationContext());
-//                imageView.setScaleType(ImageView.ScaleType.FIT_CENTER);
-//
-//                return imageView;
-//            }
-//        });
+
+
 
     }
 
-//    private void getFotos(String idLugar){
-//        final Task<PlacePhotoMetadataResponse> photoMetadataResponse = mGeoDataApi
-//
-//
-//    }
+
 }
