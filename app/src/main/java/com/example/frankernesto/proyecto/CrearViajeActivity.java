319afd822e79ce69e90f6e6cb783a07a1bcf5b2d
Frank11294 <franker11294@gmail.com>
2017-08-23 17:45:27 +0200

    Menu

--- app/src/main/java/com/example/frankernesto/proyecto/CrearViajeActivity.java
+++ app/src/main/java/com/example/frankernesto/proyecto/CrearViajeActivity.java
@@ -1,9 +1,12 @@
 package com.example.frankernesto.proyecto;
 
 import android.content.Intent;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MenuItem;
 
 import com.google.android.gms.common.api.Status;
 import com.google.android.gms.location.places.AutocompleteFilter;
@@ -15,22 +18,47 @@ public class CrearViajeActivity extends AppCompatActivity {
   private final String TAG = "Mi App";
   private Intent intent;
 
+  private DrawerLayout mDrawerLayout;
+  private ActionBarDrawerToggle mToogle;
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        if(mToogle.onOptionsItemSelected(item)){
+         return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_crear_viaje);
+        setTitle("INICIO");
 
         PlaceAutocompleteFragment autocompleteFragment = (PlaceAutocompleteFragment)
                 getFragmentManager().findFragmentById(R.id.place_autocomplete_fragment);
+
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+
+        mToogle = new ActionBarDrawerToggle(this,mDrawerLayout, R.string.open,R.string.close);
+
+
+        mDrawerLayout.addDrawerListener(mToogle);
+
+        mToogle.syncState();
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+
+
 
         autocompleteFragment.setOnPlaceSelectedListener(new PlaceSelectionListener() {
             @Override
             public void onPlaceSelected(Place place) {
 
                 Log.i(TAG, "Place: " + place.getName());
-
-//                double latitud=place.getLatLng().latitude;
-//                double longitud=place.getLatLng().longitude;
 
                 String id = place.getId();
                 String nombre = place.getName().toString();
@@ -51,5 +79,10 @@ public class CrearViajeActivity extends AppCompatActivity {
                 Log.i(TAG, "An error occurred: " + status);
             }
         });
+
+
     }
+
+
+
 }
